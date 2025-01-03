e653f420c5b64b90df1f8ccc046ecd632a1b1381
LOQ <LOQ@DESKTOP-ROCA4IP>
2025-01-03 19:44:54 +0200

    Add project files.

---
+++ task2D01/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace task2D01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Num = 6;
+            Console.WriteLine($"binary of: {Num} = {Num:B}");
+        }
+    }
+}
---
+++ taskD01/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace taskD01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int smallCarpets, largeCarpets;
+            int priceSmallCarpet = 25;
+            int priceLargeCarpet = 35;
+            int cost = 0;
+            double totalCost = 0;
+            double tax = 0.06;
+            Console.WriteLine("==== Welcome to Islam's Carpet Cleaning Service =====");
+            Console.WriteLine("Estimate for carpet cleaning service");
+            Console.Write("Number of small carpets: ");
+            smallCarpets = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Number of large carpets: ");
+            largeCarpets = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Price per small carpet:{priceSmallCarpet:C} ");
+            Console.WriteLine($"Price per large carpet:{priceLargeCarpet:C}");
+            cost = smallCarpets * priceSmallCarpet + largeCarpets * priceLargeCarpet;
+            tax = tax * cost;
+            totalCost = cost + tax;
+            Console.WriteLine($"Cost:{cost:C}");
+            Console.WriteLine($"Tax:{tax:C}");
+            Console.WriteLine("=============================");
+            Console.WriteLine($"Total Estimate:{totalCost:C}");
+            Console.WriteLine("This estimate is valid for 30 days");
+        }
+    }
+}
